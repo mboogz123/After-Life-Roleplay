6f466ae46f63d755621185c3aafdc3cb330c2307
mboogz123 <makiah.edwards@icloud.com>
2026-09-01 20:18:17 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,8 +24,8 @@ export const botConfig = {
     activities: [
       {
         name: "After Life Community", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
-        type: 3,               // Custom
+        state: "Watching",     // this is what people actually see
+        type: 4,               // Custom
       },
     ],
   },

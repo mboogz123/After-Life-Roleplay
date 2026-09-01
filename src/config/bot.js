6f19ec2a6718ac5eba2e5b0904c695dc39b002db
mboogz123 <makiah.edwards@icloud.com>
2026-09-01 18:28:27 +0100

    Update bot status and primary color

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,9 +23,9 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "After Life Community", // required by Discord API, not shown in the client
         state: "stalking",     // this is what people actually see
-        type: 4,               // Custom
+        type: 3,               // Custom
       },
     ],
   },
@@ -93,7 +93,7 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
+      primary: "#e3ffcb",
       secondary: "#2F3136",
 
       // Standard status colors for success/error/warning/info messages.

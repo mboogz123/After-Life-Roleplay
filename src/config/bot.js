de1fa8ae323db648c8720afd23ab34a79cef3c16
mboogz123 <makiah.edwards@icloud.com>
2026-09-03 23:50:25 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "After Life Community", // required by Discord API, not shown in the client
+        name: "ATM Community", // required by Discord API, not shown in the client
         state: "Watching",     // this is what people actually see
         type: 4,               // Custom
       },
